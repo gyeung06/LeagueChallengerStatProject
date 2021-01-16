9020a9ac28b424974962ee8a25b5b44e5a91b094
Gabriel Yeung <gabriel.yeung719@gmail.com>
2021-01-16 04:26:31 -0800

    Create CSV file with all challenger players Works in Tableau

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pantheon import pantheon
+import pandas as pd
 import asyncio
 
 server = "na1"
 
 #Put in own dev key to test
-api_key = "RGAPI-xxx"
+api_key = "RGAPI-7f7f0325-a14c-42ad-ba19-75068399ced7"
 
 
 def requestLog(url, status, headers):
@@ -16,20 +17,37 @@ def requestLog(url, status, headers):
 panth = pantheon.Pantheon(server, api_key, errorHandling=True, requestsLoggingFunction=requestLog, debug=True)
 
 
-async def getSummonerId(name):
+async def getChall():
     try:
-        data = await panth.getSummonerByName(name)
-        return data['id'], data['accountId']
+        data = await panth.getChallengerLeague("RANKED_SOLO_5x5")
+        return data['entries']
     except Exception as e:
         print(e)
 
 
-name = "FunOnDaBun"
-
 loop = asyncio.get_event_loop()
 
-(summonerId, accountId) = loop.run_until_complete(getSummonerId(name))
+(entries) = loop.run_until_complete(getChall())
 
-print(summonerId)
-print(accountId)
+data = {}
+i = 1
+
+for summoner in entries:
+    temp = []
+    for item in summoner:
+        temp.append(summoner[item])
+    tempDict = {i: temp}
+    i = i + 1
+    data.update(tempDict)
+
+print(data)
+df = pd.DataFrame(data)
+df = df.transpose()
+df.columns = ['SummmonerID', 'SummonerName', 'Points', 'Rank', 'Wins', 'Losses', 'Veteran', 'Inactive', 'Freshblood', 'HotStreak']
+df.info()
+df.to_csv('testing.csv')
+
+
+
+
 
